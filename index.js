7806724332b6fcbd9553fea36ecbd338402f34e2
Monika <monika.mathur@meta.com>
2020-03-25 15:15:03 +0530

    add

--- index.js
+++ index.js
@@ -12,30 +12,59 @@ let formId = jsonData.schema.objtypes[0].id;
 let htmlTemplate = generateFormHeader(formId);
 
 function generateFormHeader(options) {
-    return `
+  return `
     <mbsc-form [options]="${options}">
     <mbsc-form-group>
     `
-  };
+};
 
- function  generateFormFooter() {
-    return `
+function generateFormFooter() {
+  return `
     </mbsc-form-group>
     <mbsc-button>Submit</mbsc-button>
     </mbsc-form>
     `
-  }
-  function generateInput(ngModel , placeholder){
-    return `
+}
+function generateInput(ngModel, placeholder) {
+  return `
     <mbsc-input
     [(ngModel)]="${ngModel}"
     [placeholder]="${placeholder}"
     ></mbsc-input>
     `
-  }
+}
 
-  function  generateSelect(ngModel , options, data, onSet){
-    return `
+function generatePassword(ngModel, type, placeholder) {
+  return `
+    <mbsc-input
+     [(ngModel)]="${ngModel}"
+     type="${type}"
+     placeholder="${placeholder}">
+     </mbsc-input>
+    `
+}
+
+function generateTextArea(ngModel, placeholder) {
+  return `
+    <mbsc-textarea 
+    [(ngModel)]="${ngModel}" 
+    placeholder="${placeholder}">
+    </mbsc-textarea>
+    `
+}
+
+function generateEmail(ngModel, type, placeholder) {
+  return `
+    <mbsc-input 
+    [(ngModel)]="${ngModel}" 
+    type="${type}" 
+    placeholder="${placeholder}">
+    </mbsc-input>
+    `
+}
+
+function generateSelect(ngModel, options, data, onSet) {
+  return `
     <mbsc-select
     [(ngModel)]="${ngModel}"
     [options]="${options}"
@@ -43,14 +72,38 @@ function generateFormHeader(options) {
     (onSet)="${onSet}"
     ></mbsc-select>
     `
-  }
+}
+
+function generateRadio(ngModel,data, type, placeholder) {
+let radioGroupStart =  `<mbsc-radio-group 
+[(ngModel)]="${ngModel}">`;
+   for(let i = 0; i < data.length-1; i++){
+     let temp = `<mbsc-radio 
+     [value]="${data[i].value}">
+     </mbsc-radio>
+     `
+     let radioGroupStart =  `<mbsc-radio-group 
+[(ngModel)]="${ngModel}">`;
+radioGroupStart =  radioGroupStart+ temp;
+   }
+  return `
+    <mbsc-radio-group 
+    [(ngModel)]="${ngModel}">
+    <mbsc-radio 
+    [value]="x[0].value">
+    </mbsc-radio> 
+    <mbsc-radio [value]="x[1].value">{{x[1].label}}</mbsc-radio>
+    </mbsc-radio-group>
+    `
+}
+
 
 properties.forEach(element => {
   let temp;
-  if(element.componentref.id == "input"){
-   temp = generateInput(formId+'.'+element.id, formId+'.placeholder',)
-  }else if(element.componentref.id == "select"){
-  temp =  generateSelect(formId+'.'+element.id, '', element.enumvals, formId+'.'+element.id+'.onSelect($event)')
+  if (element.componentref.id == "input") {
+    temp = generateInput(formId + '.' + element.id, formId + '.placeholder')
+  } else if (element.componentref.id == "select") {
+    temp = generateSelect(formId + '.' + element.id, '', element.enumvals, formId + '.' + element.id + '.onSelect($event)')
   }
   htmlTemplate = htmlTemplate + temp;
 });
@@ -58,11 +111,11 @@ htmlTemplate = htmlTemplate + generateFormFooter();
 
 var fileName = 'index.html';
 var stream = fs.createWriteStream(fileName);
-stream.once('open', function(fd) {
-    var html = htmlTemplate;
-  
-    stream.end(html);
-  });
+stream.once('open', function (fd) {
+  var html = htmlTemplate;
+
+  stream.end(html);
+});
 
 
 console.log("Generated Form*   : ", htmlTemplate);
